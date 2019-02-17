990a8ecd75e121f070e01044a5405ab7cb75b0fa
Balazs Kelemen <10376327+prampec@users.noreply.github.com>
2019-02-17 22:25:44 +0100

    Initial commit

---
+++ src/GsmAsync.cpp
@@ -0,0 +1,278 @@
+/**
+ * GsmAsync.h -- GsmAsync is a module helps communicate with 
+ * SIM900, SIM800 modules in a non-blocking way.
+ *   https://github.com/prampec/GsmAsync
+ *
+ * Copyright (C) 2019 Balazs Kelemen <prampec+arduino@gmail.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+#include "GsmAsync.h"
+
+static char* GSMASYNC_OK_STR = "OK";
+static char* GSMASYNC_ERROR_STR = "ERROR";
+
+GsmAsync::GsmAsync(HardwareSerial* gsm, void (*timeoutHandler)(), void (*errorHandler)())
+{
+  this->_gsm = gsm;
+  this->_timeoutHandler = timeoutHandler;
+  this->_errorHandler = errorHandler;
+}
+
+void GsmAsync::doLoop()
+{
+  while (this->_gsm->available())
+  {
+    if (this->_gsmState == GSMASYNC_READ_STATE_BUFF)
+    {
+      // -- Read result to buffer on handler-match
+      boolean bufferReady = this->fillResultBuffer();
+      if (bufferReady)
+      {
+        (this->_handlerToCall->callback)(this->_buffer); // -- Call callback.
+        this->_gsmState = GSMASYNC_READ_STATE_NORMAL;
+      }
+    }
+    else // -- GSMASYNC_READ_STATE_NORMAL
+    {
+      // -- Read all characters and search for a pattern match.
+      char c = this->_gsm->read();
+//Serial.write(c);
+      if (c < 32)
+      {
+        this->_okPos = 0;
+        this->_errorPos = 0;
+        continue;
+      }
+      if (checkOk(c))
+      {
+        // TODO: might want to clear read buffer before calling any new command
+        this->handleOk();
+        continue;
+      }
+      if (checkError(c))
+      {
+        // TODO: might want to clear read buffer before calling any new command
+        this->handleError();
+        continue;
+      }
+      this->_handlerToCall = checkGsmHandler(c);
+      if (this->_handlerToCall != NULL)
+      {
+        this->_buffPos = 0;
+        this->_gsmState = GSMASYNC_READ_STATE_BUFF;
+      }
+    }
+  }
+
+  this->checkTimeout();
+}
+
+// -- TODO: How could we add OK and ERROR handlers as GsmHandler?
+/**
+ * Retruns true, when OK symbole received.
+ */
+boolean GsmAsync::checkOk(char c)
+{
+  if (this->_okPos < 0)
+  {
+    return false; // -- Start check after new line
+  }
+  if (GSMASYNC_OK_STR[this->_okPos] == c)
+  {
+    this->_okPos += 1;
+    if (this->_okPos == strlen(GSMASYNC_OK_STR))
+    {
+      this->_okPos = 0;
+      return true;
+    }
+    return false;
+  }
+
+  this->_okPos = -1;
+  return false;
+}
+/**
+ * Retruns true, when OK symbole received.
+ */
+boolean GsmAsync::checkError(char c)
+{
+  if (this->_errorPos < 0)
+  {
+    return false; // -- Start check after new line
+  }
+  if (GSMASYNC_ERROR_STR[this->_errorPos] == c)
+  {
+    this->_errorPos += 1;
+    if (this->_errorPos == strlen(GSMASYNC_ERROR_STR))
+    {
+      this->_errorPos = 0;
+      return true;
+    }
+    return false;
+  }
+
+  this->_errorPos = -1;
+  return false;
+}
+
+/**
+ * Retruns true, when buffer is ready.
+ */
+boolean GsmAsync::fillResultBuffer()
+{
+  while (this->_gsm->available())
+  {
+    char c = this->_gsm->read();
+    if ((this->_buffPos == 0) && (c == ' '))
+    {
+      continue; // Swallow empty characters between prefix and result.
+    }
+    if ((c < 32) || (this->_buffPos >= (sizeof(this->_buffer)-1)))
+    {
+      this->_buffer[this->_buffPos] = '\0';
+      return true;
+    }
+    this->_buffer[this->_buffPos++] = c;
+  }
+  return false;
+}
+
+void GsmAsync::registerHandler(GsmHandler* handler)
+{
+  if (this->_firstHandler == NULL)
+  {
+    this->_firstHandler = handler;
+    return;
+  }
+  GsmHandler* lastHandler = this->_firstHandler;
+  while(lastHandler->nextHandler != NULL)
+  {
+    lastHandler = lastHandler->nextHandler;
+  }
+  lastHandler->nextHandler = handler;
+}
+
+/**
+ * Returns the handler, when match is found.
+ */
+GsmHandler* GsmAsync::checkGsmHandler(char c)
+{
+  GsmHandler* handler = this->_firstHandler;
+  while(handler != NULL)
+  {
+    if (handler->prefix[handler->matchPos] != c)
+    {
+      handler->matchPos = 0;
+    }
+    if (handler->prefix[handler->matchPos] == c)
+    {
+      // -- Has a match!
+      handler->matchPos += 1;
+      if (handler->prefix[handler->matchPos] == '\0')
+      {
+        // -- prefix matches with the string
+        this->resetAllMatches();
+        return handler;
+      }
+    }
+    handler = handler->nextHandler;
+  }
+  return NULL;
+}
+
+/**
+ * Resets all handlers to match-start position.
+ */
+void GsmAsync::resetAllMatches()
+{
+  GsmHandler* handler = this->_firstHandler;
+  while(handler != NULL)
+  {
+    handler->matchPos = 0;
+    handler = handler->nextHandler;
+  }
+}
+
+void GsmAsync::handleOk()
+{
+  if (this->_nextCommand > 0)
+  {
+    for(int i=1; i < this->_nextCommand; i++)
+    {
+      this->_commands[i-1] = this->_commands[i];
+      this->_commandTimeouts[i-1] = this->_commandTimeouts[i];
+    }
+    this->_nextCommand -= 1;
+  }
+  this->_waitingForResponse = false;
+  this->_retryCount = 0;
+//Serial.println("OK");
+  this->executeNextCommand();
+}
+
+void GsmAsync::handleError()
+{
+  if (this->_errorHandler)
+  {
+    this->_errorHandler();
+  }
+}
+
+void GsmAsync::addCommand(char* command, unsigned long timeOutMs)
+{
+  if (this->_nextCommand >= GSMASYNC_COMMAND_BUF_SIZE)
+  {
+    // TODO: generate error
+    return;
+  }
+  this->_commands[this->_nextCommand] = command;
+  this->_commandTimeouts[this->_nextCommand] = timeOutMs;
+  this->_nextCommand += 1;
+  if (this->_nextCommand == 1)
+  {
+    // -- If this is the first command, immediatelly execute it.
+    executeNextCommand();
+  }
+}
+
+void GsmAsync::executeNextCommand()
+{
+  if (this->_nextCommand > 0)
+  {
+    const char* currentCommand = this->_commands[0];
+//Serial.println(currentCommand);
+    this->_gsm->println(currentCommand);
+    this->_waitingForResponse = true;
+    this->_lastSendTime = millis();
+  }
+}
+
+void GsmAsync::checkTimeout()
+{
+  if (this->_waitingForResponse)
+  {
+    unsigned long currentCommandTimeout = this->_commandTimeouts[0];
+    if (currentCommandTimeout < (millis() - _lastSendTime))
+    {
+      // -- Time Out
+      this->_retryCount += 1;
+      if (this->_retryCount >= GSMASYNC_MAX_RETRIES)
+      {
+        // -- Retries exceeded
+        // -- Clear commands
+        this->_nextCommand = 0;
+        this->_retryCount = 0;
+        if (this->_timeoutHandler != NULL)
+        {
+          this->_timeoutHandler(); // -- Call handler
+        }
+        return;
+      }
+      this->executeNextCommand();
+    }
+  }
+}
+
---
+++ src/GsmAsync.h
@@ -0,0 +1,96 @@
+/**
+ * GsmAsync.h -- GsmAsync is a module helps communicate with 
+ * SIM900, SIM800 modules in a non-blocking way.
+ *   https://github.com/prampec/GsmAsync
+ *
+ * Copyright (C) 2019 Balazs Kelemen <prampec+arduino@gmail.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+#ifndef GsmAsync_h
+#define GsmAsync_h
+
+#include <Arduino.h>
+
+#define GSMASYNC_BUF_SIZE 32
+#define GSMASYNC_COMMAND_BUF_SIZE 20
+#define GSMASYNC_READ_STATE_NORMAL 0
+#define GSMASYNC_READ_STATE_BUFF 1
+#define GSMASYNC_REPONSE_TIMEOUT_MS 300
+#define GSMASYNC_MAX_RETRIES 3
+
+/**
+ * Structure for holding response callbacks.
+ */
+typedef struct GsmHandler
+{
+  const char* prefix;
+  void (*callback)(char* response);
+  byte matchPos; // -- For internal use only
+  GsmHandler* nextHandler; // -- For internal use only
+} GsmHandler;
+
+/**
+ * Main class of the module.
+ */
+class GsmAsync
+{
+  public:
+    /**
+     * Create the GsmAsync instance.
+     * gsm - Reference to the serail line
+     * timeoutHandler - This method will be called, when a command is sent GSMASYNC_MAX_RETRIES times 
+     *     and still still no OK answer arrived within timeOutMs from the module.
+     * errorHandler - This method will be called, when ERROR response arrives from the module.
+     */
+    GsmAsync(HardwareSerial* gsm, void (*timeoutHandler)() = NULL, void (*errorHandler)() = NULL);
+
+    /**
+     * The callback will be called, when string match found.
+     */
+    void registerHandler(GsmHandler* handler);
+
+    /**
+     * Add a new command to the command-queue.
+     * If the queue was previously empty, the command will be immediatelly executed.
+     */
+    void addCommand(char* command, unsigned long timeOutMs = GSMASYNC_REPONSE_TIMEOUT_MS);
+
+    /**
+     * Call this as often as possible, to check for new incoming data, and timeouts.
+     */
+    void doLoop();
+
+  private:
+    boolean fillResultBuffer();
+    GsmHandler* checkGsmHandler(char c);
+    boolean checkOk(char c);
+    boolean checkError(char c);
+    void resetAllMatches();
+    void executeNextCommand();
+    void checkTimeout();
+    void handleOk();
+    void handleError();
+    
+    HardwareSerial* _gsm;
+    void (*_timeoutHandler)();
+    void (*_errorHandler)();
+    char _buffer[GSMASYNC_BUF_SIZE];
+    GsmHandler* _firstHandler = NULL;
+    GsmHandler* _handlerToCall;
+    char* _commands[GSMASYNC_COMMAND_BUF_SIZE];
+    unsigned long _commandTimeouts[GSMASYNC_COMMAND_BUF_SIZE];
+    byte _buffPos = 0;
+    byte _gsmState = GSMASYNC_READ_STATE_NORMAL;
+    char _okPos = -1;
+    char _errorPos = -1;
+    byte _nextCommand;
+    boolean _waitingForResponse = false;
+    unsigned long _lastSendTime;
+    byte _retryCount = 0;
+};
+
+#endif
+
